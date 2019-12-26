de6e985f6f992c8fca5ee0e2b12bc6c005fe53f7
Rasmus Thomsen <oss@cogitri.dev>
2019-12-26 14:31:09 +0100

    chore(meson): pass -main to the dcompiler instead of having a dummy D main function.

--- source/corecollector/tests/main.d
+++
@@ -1,6 +0,0 @@
-/// Dummy main file. Since this is a library we don't provide a main()
-/// usually, but we need one for the unittests. `DFLAGS="-main"` leads
-/// to weird linking errors, so just do this for now.
-void main()
-{
-}
